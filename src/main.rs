be879917af7fcc8fc977f77ed0c0560a73bceae3
AnushaTMF <anushaupreti99@gmail.com>
2023-06-07 22:14:53 +0545

    first version of project

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use mini_grep::{run, Config};
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("problem parsing arguments: {err}");
+        process::exit(1)
+    });
+    // println!("{:?}",config);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error:{e}");
+        process::exit(1)
+    };
+}
